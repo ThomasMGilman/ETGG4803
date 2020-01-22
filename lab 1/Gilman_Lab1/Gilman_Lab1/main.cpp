2983cfcaef21a38d9686f6fd3f4389d3a0e5035a
ThomasMGilman <Thomas.Gilman@ymail.com>
2020-01-22 14:05:37 -0500

    start of artificial intelligence class, start with lab 1 bridge tourch problem

---
+++ lab 1/Gilman_Lab1/Gilman_Lab1/main.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+class pairTree
+{
+	public:
+		pairTree(std::vector<int> From, std::vector<int> To)
+		{
+
+		}
+
+	private:
+
+};
+
+
+// People to get across bridge
+std::vector<int> From = {1, 4, 6, 20};
+std::vector<int> To;
+int main(int argc, const char* argv[])
+{
+
+}
